cbe042d229b5f0546aec3026aaa6190e121f4163
emdee52 <emdee7101@gmail.com>
2023-02-13 17:49:30 -0700

    added buyingChance to Buyer

--- Project 2/src/Buyer.java
+++ Project 2/src/Buyer.java
@@ -2,16 +2,32 @@ import java.util.SplittableRandom;
 
 public class Buyer {
     SplittableRandom random = new SplittableRandom();
-    private String buyingType;
+    private String buyerType;
     private String vehicleType;
+    private double buyingChance;
 
     Buyer() {
         vehicleType = new String[]{"Performance", "Car", "Pickup"}[random.nextInt(3)];
-        buyingType = new String[]{"Just Looking", "Wants One", "Needs One"}[random.nextInt(3)];
+        buyerType = new String[]{"Just Looking", "Wants One", "Needs One"}[random.nextInt(3)];
+        switch (buyerType) { // calculate base chance to buy based on buyerType
+            case "Just Looking":
+                buyingChance = 10.0; // 10%
+                break;
+            case "Wants One":
+                buyingChance = 40.0; // 40%
+                break;
+            case "Needs One":
+                buyingChance = 70.0; // 70%
+                break;
+            default:
+                buyingChance = 0; // should never be reached
+                break;
+        }
     }
 
     public void report() {
-        System.out.println("VehicleType: " + vehicleType);
-        System.out.println("BuyingType:  " + buyingType);
+        System.out.println("\nVehicleType:  " + vehicleType);
+        System.out.println("BuyerType:    " + buyerType);
+        System.out.println("ChanceToBuy:  " + buyingChance);
     }
 }
